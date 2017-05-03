a31c982ea77e8c6c720b7866c84b3f431208b670
taihon <taihon@users.noreply.github.com>
2017-05-03 20:56:03 +0700

    Cat class, and tests for feature requests to it's implementation

--- CodersCat.Model/Cat.cs
+++ CodersCat.Model/Cat.cs
@@ -4,9 +4,50 @@ namespace CodersCat.Model
 {
     public class Cat
     {
+        private int Heath = 6;
+        public string Color {
+            get {
+                if(Heath < 3) { return "Red"; }
+                /* в ТЗ сказано, что желтый цвет должен быть при здоровье
+                 * равном 5, а цвет кошки при значениях 3, 4 не определён
+                 * Изменил условие, желтой кошка становится при здоровье
+                 * от 3 до 5 включительно
+                 */
+                if (Heath <=5&& Heath>=3) { return "Yellow"; }
+                return "Green";
+            }
+        }
+
         public Cat(int age)
         {
-            throw new ArgumentOutOfRangeException();
+            if(age <= 0)
+                throw new ArgumentOutOfRangeException();
+            Age = age;
+        }
+        private string name;
+        public int Age { get; internal set; }
+        public string Name { get { return name; } set {
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    name = value;
+                } else
+                {
+                    throw new InvalidOperationException("You should set name only once!");
+                }
+            }
+        }
+
+        public void Punish()
+        {
+            if(Heath > 1) { 
+                Heath--;
+            }
+            else { throw new InvalidOperationException("You cannot punish cat to death!"); }
+        }
+
+        public void Feed()
+        {
+            Heath++;
         }
     }
 }
--- CodersCat.Tests/Model.cs
+++ CodersCat.Tests/Model.cs
@@ -8,18 +8,94 @@ namespace CodersCat.Tests
     [TestClass]
     public class Model
     {
-        private Cat cat;
         [TestMethod]
-        public void CreateCatWithAgeEqualToZeroThrows()
+        public void CreateCatWithAgeEqualToZeroThrowsArgumentOutOfRange()
         {
             Action action = () => new Cat(0);
-            action.ShouldThrow<ArgumentOutOfRangeException>();
+            action.ShouldThrow<ArgumentOutOfRangeException>("user shouldn't be able to create Cats with age equal to zero");
         }
         [TestMethod]
         public void CreateWithPositiveAgeNotThrows()
         {
             Action action = () => new Cat(1);
-            action.ShouldNotThrow<ArgumentOutOfRangeException>();
+            action.ShouldNotThrow<ArgumentOutOfRangeException>("user should be able to create Cats with age greater than zero");
+        }
+        [TestMethod]
+        public void CreateWithNegativeAgeThrowsArgumentOutOfRange()
+        {
+            Action action = () => new Cat(-1);
+            action.ShouldThrow<ArgumentOutOfRangeException>("user shouldn't be able to create Cats with negative age");
+        }
+        [TestMethod]
+        public void CreateCatWithAgeOfOneSetsAgeProperty()
+        {
+            var cat = new Cat(1);
+            cat.Age.Should().Be(1, "age of Cat(1) should be equal to 1");
+        }
+        [TestMethod]
+        public void CreateCatWithAgeOfTwoSetsAgeToTwo()
+        {
+            var cat = new Cat(2);
+            cat.Age.Should().Be(2, "age of Cat(2) should be equal to 2");
+        }
+        [TestMethod]
+        public void CatColorIsGreenByDefault()
+        {
+            var cat = new Cat(1);
+            cat.Color.Should().Be("Green", "healthy Cat color is green");
+        }
+        [TestMethod]
+        public void CatChangesColorToYellowWhenPunishedJustAfterCreation()
+        {
+            var cat = new Cat(2);
+            cat.Punish();
+            cat.Color.Should().Be("Yellow", "cat should change color to yellow when hit just after creation");
+        }
+        [TestMethod]
+        public void CatChangesColorToRedWhenHeathBelowThree()
+        {
+            var cat = new Cat(2);
+            for(int i = 0; i < 4; i++)
+            {
+                cat.Punish();
+            }
+            cat.Color.Should().Be("Red", "cat should have red color when Heath below 3");
+        }
+        [TestMethod]
+        public void FeedingCatIncreasesHeath()
+        {
+            var cat = new Cat(2);
+            cat.Punish();
+            cat.Feed();
+            cat.Color.Should().Be("Green", "feeding cat should increase Heath, and change color when appropriate");
+        }
+        [TestMethod]
+        public void PunishingCatShouldNotLowerHeathBelowOne()
+        {
+            var cat = new Cat(1);
+            for (int i = 0; i < 5; i++) { cat.Punish(); }
+            Action a = () => cat.Punish();
+            a.ShouldThrow<InvalidOperationException>("Heath equal to zero is not allowed")
+                .WithMessage("You cannot punish cat to death!");
+        }
+        [TestMethod]
+        public void UserAbleToSetNameForCat()
+        {
+            var cat = new Cat(1)
+            {
+                Name = "Fat Sally"
+            };
+            cat.Name.Should().Be("Fat Sally", "user should be able to set cat's name after creation");
+        }
+        [TestMethod]
+        public void UserUnableToChangeNameAfterAssigningOne()
+        {
+            var cat = new Cat(1)
+            {
+                Name = "Fat Sally"
+            };
+            Action a = () => cat.Name = "Thin Sally";
+            a.ShouldThrow<InvalidOperationException>().WithMessage("You should set name only once!");
         }
     }
 }
